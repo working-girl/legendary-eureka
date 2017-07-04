a76c2285db97200b86f708299753b13471de08f5
working-girl <natespeh@gmail.com>
2017-07-04 11:04:31 +0200

    added a landing page with search component and logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: SearchbarComponent
   },
   {
   	path: 'home',
